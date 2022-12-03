bdf65d5fd02af77bc6c4c090c820b38b988c6794
Alberto <padoalberto@gmail.com>
2022-12-03 18:12:33 +0100

    Autostart sway.

--- setup.sh
+++ setup.sh
@@ -45,4 +45,9 @@ sleep 10s
 	sudo mkdir /etc/sysctl.d/
 	echo 'vm.swappiness=10' | sudo tee /etc/sysctl.d/99-swappiness.conf
 
+    echo "# If running from tty1 start sway
+        if [ "$(tty)" = \"/dev/tty1\" ]; then
+            exec sway
+        fi" >> .bashrc
+
 clear
