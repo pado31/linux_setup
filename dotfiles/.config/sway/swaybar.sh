9a48475099a52b6590015bd8f98d73149cb43cf0
Alberto <ap@thinkpado.lan>
2022-12-09 20:24:30 +0100

    Swaybar with correct date and time. Battery percentage.

--- dotfiles/.config/sway/swaybar.sh
+++ dotfiles/.config/sway/swaybar.sh
@@ -3,11 +3,10 @@
 # The Sway configuration file in ~/.config/sway/config calls this script.
 
 # The abbreviated weekday (e.g., "Fri"), followed by the ISO-formatted date
-date_formatted=$(date +'%a %F %H:%M')
+date_formatted=$(date -u +'%a %F %H:%M')
 
-# TODO: Improve
 # Returns the battery status: "Full", "Discharging", or "Charging".
-battery_status=$(cat /sys/class/power_supply/BAT0/status)
+battery_status=$(cat /sys/class/power_supply/BAT0/capacity)%
 
 # Emojis and characters for the status bar
-echo $uptime_formatted $battery_status $date_formatted
+echo $date_formatted $battery_status 
